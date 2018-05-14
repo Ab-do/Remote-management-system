b525bbb87c3990b5c01d94995f1001191aa32d0e
abdo <abdo.bougaa@gmail.com>
2018-05-14 02:00:27 +0100

    WC10

--- wc/wc.ino
+++ wc/wc.ino
@@ -1,5 +1,5 @@
 // WC 8 : wirless controller.
-// le 04/03/2019 Agadir.
+// le 04/03/2018 Agadir.
 #include <SoftwareSerial.h>
 #include<EEPROM.h>
 const int LED_START=3;
@@ -7,10 +7,12 @@ const int LED_WIRLESS_COM=2;
 const int LED_CHECK_GSM=4;
 const int AD_PHONE=10;
 const int AD_SETTING_SMS=30;
+const int AD_CONT_SMS=60;
 String Phone="";
 int numberPhone[9]={0};        // nombre des objets de systeme
 int settingSMS[6]={0};
 int GsmSgnal=0;
+int ContSMS=0;
 const int MTR=15;
 unsigned long Last=millis();
 unsigned long CheckSMS=millis();
@@ -32,7 +34,7 @@ void setup() {
   Serial3.println("AT+CMGF=1");
   Serial3.println("AT+CNMI=1,2,0,0,0");
   digitalWrite(LED_START,HIGH);
-  //Serial3.println("AT+CMGD=1,4");
+  Serial3.println("AT+CMGD=1,4");
   sendSMS("GB-9467",1);
   //switchSMS("2"); // tm
 }
@@ -44,7 +46,7 @@ void loop() {
   getDataSerial();
 }
 
-//  obtenir les donnée 
+//  obtenir les donnée
 void getDataSerial(){
   if(Serial.available()>0){
     String data=Serial.readString();
@@ -73,9 +75,12 @@ void getDataHc(){
           checkNet(true);
          }else if(data[1]=='d'){
           sendSMS("GB-9478",1);
-          checkWirless();
           checkNet(true);
          }else if(data[1]=='e'){
+          checkConx();
+         }else if(data[1]=='r'){
+          ContSMS=0;
+          EEPROM.put(AD_CONT_SMS,ContSMS);
           checkConx();
          }
       break;
@@ -95,10 +100,10 @@ void getDataHc(){
           //switchSMS("1"+data);
           if(SMS_Responce==true && millis()-CheckSMS<20000 && settingSMS[1]!=1){
                 Serial.println("SMS send");
-                sendSMS(data,1);
+                sendSMS("Gx21"+data,1);
                 SMS_Responce==false;
            }else {
-                sendSMS(data,settingSMS[1]);
+                sendSMS("Gx21"+data,settingSMS[1]);
                 SMS_Responce==false;
            }
       break;
@@ -134,25 +139,25 @@ void filtering(String str){
         int j=0;
           while(str[i]!=62){
             dataSend +=str[i];
-            j++; 
+            j++;
             i++;
             if(i>str.length()) return 0;
          }
          //Commutation des données.
          //showMatrix(Matrix,20);
          //switchData(Matrix);
-         Serial.println(dataSend);
-         Serial2.println(dataSend);
+         //Serial.println(dataSend);
+         Serial2.println("<"+dataSend+">");
          SMS_Responce=true;
          CheckSMS=millis();
          //memset(Matrix,0,sizeof(Matrix));
       }
       else{
-         i++; 
-      }     
+         i++;
+      }
   }
 }
-//switchSMS 
+//switchSMS
 void switchSMS(String str){
   Serial.print("les données ...");
   Serial.println(str);
@@ -182,10 +187,10 @@ void switchSMS(String str){
        sendSMS(msg,1);
     break;
     case 4:
-      
+
     break;
   };
-  
+
 }
 
 //fonction set nemero de telephone du client
@@ -205,6 +210,7 @@ void loadingData(){
   EEPROM.get(AD_PHONE,numberPhone);
   Phone=toString(numberPhone);
   EEPROM.get(AD_SETTING_SMS,settingSMS);
+  EEPROM.get(AD_CONT_SMS,ContSMS);
   Serial.println(Phone);
   for(int i=0;i<4;i++) {
     Serial.print(settingSMS[i]);
@@ -215,21 +221,19 @@ void loadingData(){
 
 ///// Envoie des notifications et des informations par SMS.
 void sendSMS(String outMessage,int validity){
-  Serial.print("SMS: ");
-  Serial.println(outMessage);
   if(validity==1){
-  Serial3.print("AT+CMGF=1\r");
+  Serial3.println("AT+CMGF=1");
   delay(500);
   if(Phone=="000000000"){
     Phone="669600729";
   }
-  Serial3.println("AT+CMGS=\"+212"+Phone+"\"");
+  Serial3.println("AT+CMGS=\"+212669600729\"");
   delay(500);
   Serial3.println(outMessage);
   delay(500);
   Serial3.write((char)26); //ctrl+z
   delay(500);
-  Serial3.println("AT+CMGD=1,4");
+  contSMS();
   }
 }
 
@@ -238,7 +242,7 @@ String toString(int Matrix[9]){
   String str="";
   for(int i=0;i<9;i++)
     str+=Matrix[i];
-  return str; 
+  return str;
 }
 
 void smsExmple(int Matrix[]){
@@ -322,7 +326,7 @@ void checkGsm(){
         }else{
            digitalWrite(LED_CHECK_GSM,HIGH);
         }
-       }     
+       }
     }else {
        Serial2.println("<8200>");
        digitalWrite(LED_CHECK_GSM,LOW);
@@ -342,9 +346,11 @@ void checkNet(bool mode){
 }
 
 void checkConx(){
-  String msg="GB-911";
+  String msg="GB-91";
   msg+=toString(GsmSgnal);
+  msg+=toStringPin(ContSMS);
   sendSMS(msg,1);
+  Serial2.println(msg);
 }
 void restSys(){
     for (int i = 1 ; i < EEPROM.length() ; i++) {
@@ -352,3 +358,32 @@ void restSys(){
   }
   setup();
   }
+
+String toString(int value){
+  if(value<10){
+    return "0"+String(value);
+  }else {
+    return String(value);
+  }
+}
+String toStringPin(int value){
+  if(value<10){
+    return   "000"+String(value);
+  } if(value <100) {
+     return "00"+String(value);
+  } if(value <1000) {
+     return  "0"+String(value);
+  }else {
+    return String(value);
+  }
+}
+
+void contSMS(){
+  if(ContSMS<9999){
+    ContSMS++;
+  }else {
+    ContSMS=1;
+  }
+  EEPROM.put(AD_CONT_SMS,ContSMS);
+  Serial3.println("AT+CMGD=1,4");
+}
